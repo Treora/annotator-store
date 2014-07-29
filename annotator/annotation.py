8c3be444afc33052d349724a3a7d9df9314cbe17
Gerben <gerben@treora.com>
2014-07-28 19:21:10 -0700

    Small edits&fixes

--- annotator/annotation.py
+++ annotator/annotation.py
@@ -101,7 +101,7 @@ class Annotation(es.Model):
         # The JSON-LD spec recommends to put @context at the top of the
         # document, so we'll be nice and use and ordered dictionary.
         annotation = OrderedDict()
-        annotation['@context'] = context,
+        annotation['@context'] = context
         annotation['@id'] = self['id']
         annotation['@type'] = 'oa:Annotation'
         annotation['oa:hasBody'] = self.has_body
@@ -128,8 +128,7 @@ class Annotation(es.Model):
             # Note that we treat an empty text as not having text at all.
             return []
         body = {
-            '@type': 'dctypes:Text',
-            '@type': 'cnt:ContentAsText',
+            '@type': ['dctypes:Text', 'cnt:ContentAsText'],
             'dc:format': 'text/plain',
             'cnt:chars': self['text'],
         }
@@ -142,8 +141,7 @@ class Annotation(es.Model):
             return []
         return [
             {
-                '@type': 'oa:Tag',
-                '@type': 'cnt:ContentAsText',
+                '@type': ['oa:Tag', 'cnt:ContentAsText'],
                 'dc:format': 'text/plain',
                 'cnt:chars': tag,
             }
@@ -158,9 +156,9 @@ class Annotation(es.Model):
         """
         motivations = []
         if self.textual_bodies:
-            motivations.append({'@id': 'oa:commenting'})
+            motivations.append('oa:commenting')
         if self.tags:
-            motivations.append({'@id': 'oa:tagging'})
+            motivations.append('oa:tagging')
         return motivations
 
     @property
